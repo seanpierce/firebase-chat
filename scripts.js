c4126088abf9ddc3b3613f11787bb6573da6a24f
seanpierce <sumler.sean@gmail.com>
2017-11-11 14:12:36 -0700

    big ole push

---
+++ scripts.js
@@ -0,0 +1,86 @@
+$(() => {
+  // create ref
+  const ref = firebase.database().ref('messages/')
+
+  // time in milliseconds
+  const fiveMinutes = 300000
+
+  const sendMessage = (username) => {
+    // collect input
+    const message = $('#message-input').val()
+    // push new message ref
+    let newMessage = ref.push()
+    // update ref with content
+    newMessage.set({
+      username: username,
+      content: message,
+      createdAt: Date.now()
+    })
+    //clear input
+    $('#message-input').val('')
+    // force view to bottom of div
+    $('#messages').scrollTop($('#messages')[0].scrollHeight)
+  }
+
+  // get current user (if exists)
+  let currentUser
+  firebase.auth().onAuthStateChanged(user => {
+    currentUser = user
+  })
+
+  // database SNAPSHOT
+  ref.on('value', snapshot => {
+
+    // delete old messages
+    snapshot.forEach(snap => {
+      if ((Date.now() - snap.val().createdAt) > fiveMinutes) {
+        // removes old messages from db
+        ref.child(snap.key).remove();
+        // removes old messages from page
+        $(`#${snap.key}`).remove();
+      }
+    })
+
+    // clear message div
+    $('#messages').empty()
+
+    // print messages to page
+    snapshot.forEach(snap => {
+      let messages = snapshot.val()
+      $('#messages').append(
+        `<li id="${snap.key}">
+          <span class="time">${moment(snap.val().createdAt).format('h:mm')}</span>
+          <span class="username">${snap.val().username}</span>:
+          <span class="content">${snap.val().content}</span>
+        </li>`
+      )
+    })
+  })
+
+  $('#send-message').submit(e => {
+    e.preventDefault()
+
+    // user must be logged in to submit messages
+    if (currentUser) {
+      sendMessage(currentUser.displayName)
+    } else {
+      // if user not signed in,
+      // ask user for name,
+      // set name in db
+      let newUsername = prompt(`Please enter a username:`)
+      // must be at leaset 2 chars
+      if (newUsername.length > 1 && newUsername.length <= 20) {
+        firebase.auth().signInAnonymously().then(user => {
+          // username successfully set
+          user.updateProfile({
+            displayName: newUsername
+          }).then(
+            sendMessage(newUsername)
+          )
+        })
+      } else {
+        alert(`username must be between 2 and 20 characters`)
+      }
+    }
+  })
+})
